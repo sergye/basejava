7012fe0dc0c5cff23995cd45c40f10740179515c
sergye <sergye@gmail.com>
2020-02-14 08:20:00 +0200

    Realize class ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,65 @@
+import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        if (getIndex(resume.toString()) != -1) {
+            System.out.println("model.Resume " + resume.toString() + " already exist");
+        } else if (size >= 10000) {
+            System.out.println("storage.Storage overflow");
+        } else {
+            storage[size] = resume;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("model.Resume " + uuid + " not exist");
+            return null;
+        }
+        return storage[index];
     }
 
     void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("model.Resume " + uuid + " not exist");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
