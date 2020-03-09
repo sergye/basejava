08c72552181c3bb0c519e38baa4f261e2f21f519
sergye <sergye@gmail.com>
2020-03-09 16:41:31 +0200

    Add getter and setter

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -1,5 +1,8 @@
 package com.urise.webapp;
 
+import com.urise.webapp.storage.ArrayStorage;
+import com.urise.webapp.model.Resume;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -34,7 +37,7 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,5 +1,7 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.model.Resume;
+
 import java.util.Arrays;
 /**
  * Array based storage for Resumes
@@ -8,12 +10,12 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int size;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume resume) {
+    public void save(Resume resume) {
         if (getIndex(resume.toString()) != -1) {
             System.out.println("model.com.urise.webapp.model.Resume " + resume.toString() + " already exist");
         } else if (size >= storage.length) {
@@ -24,7 +26,7 @@ public class ArrayStorage {
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         int index = getIndex(uuid);
         if (index == -1) {
             System.out.println("model.com.urise.webapp.model.Resume " + uuid + " not exist");
@@ -33,7 +35,7 @@ public class ArrayStorage {
         return storage[index];
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = getIndex(uuid);
         if (index == -1) {
             System.out.println("model.com.urise.webapp.model.Resume " + uuid + " not exist");
@@ -47,11 +49,11 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
